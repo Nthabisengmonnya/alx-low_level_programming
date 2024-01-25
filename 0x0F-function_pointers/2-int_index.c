d3355b2a5ab73cddb9b449da46aa8e5edf8a8436
nthabisengmonnya <nthabisengmonnya@gmail.com>
2024-01-24 18:00:03 -0800

    int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - search for integer
+ * @array: target array holding int
+ * @size: size array
+ * @cmp: function pointer to compare  search int
+ *
+ * Return: int element match, -1 if size <=0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[1]) i != 0)
+			return (1);
+	}
+	return (-1);
+}
