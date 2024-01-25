7ab35b4a64749ccb7aed49cdea7b21f78939f404
nthabisengmonnya <nthabisengmonnya@gmail.com>
2024-01-24 16:43:24 -0800

    function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
-#include "funcion_pointers.h"
+#include "function_pointers.h"
+
 /**
  * print_name - a function that prints name
  *
  * @f: variable to hold the string name
  * @name: name of the person
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
